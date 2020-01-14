37a0312581b7f2b2fc77ad5410f527dd5c5f8fd7
tannyjoseph <32920593+tannyjoseph@users.noreply.github.com>
2020-01-14 20:00:08 +0530

    Update loginPage.java

--- app/src/main/java/com/prateek/cropdetectionapp/loginPage.java
+++ app/src/main/java/com/prateek/cropdetectionapp/loginPage.java
@@ -7,6 +7,8 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class loginPage extends AppCompatActivity {
+    Button signin;
+    TextView skip, already;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,31 @@ public class loginPage extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_page);
+        
+        signin = findViewById(R.id.signin);
+        skip = findViewById(R.id.skip);
+        already = findViewById(R.id.already);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginPage.this, SignupPage.class));
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginPage.this, SignupPage.class));
+            }
+        });
+
+        already.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginPage.this, SignupPage.class));
+            }
+        });
 
 
     }
