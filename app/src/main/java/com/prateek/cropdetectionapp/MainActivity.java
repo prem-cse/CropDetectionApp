87421a2f25b4948a05cb78038698e3374c0a6650
tannyjoseph <32920593+tannyjoseph@users.noreply.github.com>
2020-01-14 18:54:37 +0530

    Update MainActivity.java
    Front page done.

--- app/src/main/java/com/prateek/cropdetectionapp/MainActivity.java
+++ app/src/main/java/com/prateek/cropdetectionapp/MainActivity.java
@@ -5,13 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button loginButton, acceptButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        
-        System.out.println("Hello");
+
+        loginButton = findViewById(R.id.login);
+        acceptButton = findViewById(R.id.accept);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, loginPage.class));
+            }
+        });
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Great! You can now proceed to login or sign up", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         
     }
 }
