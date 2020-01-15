c6ba1defaf1c195333a639a9184c01ee1e020029
Prateek Aggarwal <aggarwalprateek72@gmail.com>
2020-01-15 13:34:58 +0530

    community section added

---
+++ app/src/main/java/com/prateek/cropdetectionapp/CommunityAdapter.java
@@ -0,0 +1,52 @@
+package com.prateek.cropdetectionapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.MyViewHolder> {
+
+    private ArrayList<CommunityDataModel> dataSet;
+
+    public CommunityAdapter(ArrayList<CommunityDataModel> dataSet){
+        this.dataSet=dataSet;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.community_binder, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        ImageView img= holder.imageView;
+        img.setImageResource(dataSet.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView= itemView.findViewById(R.id.community_plant_image);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/prateek/cropdetectionapp/CommunityData.java
@@ -0,0 +1,7 @@
+package com.prateek.cropdetectionapp;
+
+public class CommunityData {
+    static Integer[] drawableArray = {R.drawable.disone, R.drawable.disthree, R.drawable.distwo,
+            R.drawable.disfour, R.drawable.disfive};
+
+}
